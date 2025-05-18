97975443afb1593eff13a0c4885d45477683dd9a
KhanhBaoLe <khanh.le230201@vnuk.edu.vn>
2025-05-18 08:56:54 +0700

    Practice: Adding the mobile hero image

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,8 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       <div className={styles.shape} />
       {/* <AcmeLogo /> */}
-      <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
+
+
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div
@@ -35,7 +30,23 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Add Hero Images desktop Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+
+          {/*Add hero Images mobile here*/}
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
